d4f285a4f8eef4de4b858d7e7972fd53282e996b
Nicole Daniels <nzoner12@yahoo.com>
2014-07-19 21:09:08 -0400

    Fetches quotes from webpage and displays them

--- DailyQuotes/src/nld/dailyquotes/DailyQuotes.java
+++ DailyQuotes/src/nld/dailyquotes/DailyQuotes.java
@@ -1,10 +1,27 @@
 package nld.dailyquotes;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class DailyQuotes {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Document doc = Jsoup.connect("http://www.brainyquote.com/quotes/topics/topic_inspirational.html")
+				.userAgent("Chrome")
+				.get();
+		String title = doc.title();
+		Elements quotes = doc.getElementsByClass("bqQuoteLink");
+		System.out.println(title);
+		
+		for (Element specificQuote: quotes){
+			String quote = specificQuote.select("a").text();
+			System.out.println(quote + "\n");
+		}
+			
 	}
 
 }
