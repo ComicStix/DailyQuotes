ba18fa31b047efb76640c0a51203384931e8785f
Nicole Daniels <nzoner12@yahoo.com>
2014-07-20 20:02:39 -0400

    Setup GUI with title, quote, author, and acknowledgement field

--- DailyQuotes/src/nld/dailyquotes/Window.java
+++ DailyQuotes/src/nld/dailyquotes/Window.java
@@ -1,11 +1,20 @@
 package nld.dailyquotes;
 
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.BoxLayout;
+import java.awt.Color;
 
 public class Window extends JFrame {
 
@@ -32,11 +41,29 @@ public class Window extends JFrame {
 	 */
 	public Window() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 182);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel title = new JLabel("Daily Quote",JLabel.CENTER);
+		title.setBounds(155, 6, 135, 33);
+		title.setFont(new Font("Lucida Blackletter", Font.BOLD, 25));
+		contentPane.add(title);
+		
+		JLabel acknowledgement = new JLabel("Content provided by BrainyQuote");
+		acknowledgement.setForeground(Color.LIGHT_GRAY);
+		acknowledgement.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		acknowledgement.setBounds(6, 138, 167, 16);
+		contentPane.add(acknowledgement);
+		
+		JLabel quote = new JLabel("");
+		quote.setBounds(6, 54, 438, 16);
+		contentPane.add(quote);
+		
+		JLabel author = new JLabel("");
+		author.setBounds(6, 82, 214, 16);
+		contentPane.add(author);
 	}
-
 }
